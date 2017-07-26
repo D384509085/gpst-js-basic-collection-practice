df890e148f2f1a98e37e760b0f33404c023b3897
enble Du <384509085@qq.com>
2017-07-26 20:16:12 +0000

    section1-practive1

--- main/section-1/practice-1.js
+++ main/section-1/practice-1.js
@@ -1,5 +1,11 @@
 'use strict';
 
-module.exports = function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+module.exports=function collectSameElements(collectionA, collectionB) {
+    let ret=[];
+    for(let i=0;i<collectionA.length;i++){
+        if(collectionB.indexOf(collectionA[i])!=-1){
+            ret.push(collectionA[i]);
+        }
+    }
+    return ret;
 }
